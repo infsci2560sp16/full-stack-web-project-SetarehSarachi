f749845bfb4f44ee14c0a224be4faede4d054a0a
SetarehSarachi <setareh.sarachi@gmail.com>
2016-02-10 18:47:28 -0500

    changes 1.3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,14 +26,19 @@ public class Main {
     staticFileLocation("/public");
 
     //get("/hello", (req, res) -> "Hello World");
-      get("/hello", (req, res) -> {
+    get("/hello", (req, res) -> {
+          RelativisticModel.select();
+          Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+          return "E=mc^2: 12 GeV = " + m.toString();
+        });
+     /* get("/hello", (req, res) -> {
       RelativisticModel.select();
 
       String energy = System.getenv().get("ENERGY");
 
       Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
       return "E=mc^2: " + energy + " = " + m.toString();
-    });
+    });*/
 
 
     get("/", (request, response) -> {
