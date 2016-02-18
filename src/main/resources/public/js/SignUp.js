51bf6d1342ec85f2b42eceadd0ccb30cf5242762
SetarehSarachi <setareh.sarachi@gmail.com>
2016-02-17 21:15:01 -0500

    0217

---
+++ src/main/resources/public/js/SignUp.js
@@ -0,0 +1,152 @@
+//Login form validation
+function validateLoginFormOnSubmit(){
+    document.getElementById("err_username").innerHTML ="";
+    document.getElementById("err_password").innerHTML ="";
+
+        if (document.LoginForm.username.value === "") {
+            document.getElementById("err_username").innerHTML =
+                "You must enter a username.";
+            return false;
+        }
+        if (document.LoginForm.pwd.value === "") {
+            document.getElementById("err_password").innerHTML =
+                "You must enter a password.";
+            return false;
+        }
+        return true;
+
+}
+//Sign up form validation
+function validateSignUpFormOnSubmit() {
+    document.getElementById("err_username").innerHTML ="";
+    document.getElementById("err_email").innerHTML ="";
+    document.getElementById("err_password").innerHTML ="";
+    document.getElementById("err_confirmPassword").innerHTML ="";
+    document.getElementById("err_firstname").innerHTML ="";
+    document.getElementById("err_lastname").innerHTML ="";
+
+        //username validation
+       var usernameillegalChars = /\W/;
+        if (document.signUpForm.username.value === "") {
+            document.getElementById("err_username").innerHTML =
+                "You must enter a username.";
+            return false;
+        } else if ((document.signUpForm.username.value.length < 5) || (document
+            .signUpForm.username.value.length > 15)) {
+            document.getElementById("err_username").innerHTML =
+                "The username must be between 5-15 characters.";
+            return false;
+        } else if (usernameillegalChars.test(document.signUpForm.username.value)) {
+            document.getElementById("err_username").innerHTML =
+                "The username contains illegal characters.";
+            return false;
+        }
+        //email validation   
+        var emailillegalChars = /[\(\)<\>\,\;\:\\\"\[\]]/;
+        if (document.signUpForm.emailId.value === "") {
+            document.getElementById("err_email").innerHTML =
+                "You must enter your email.";
+            return false;
+        } else if (document.signUpForm.emailId.value.match(emailillegalChars)) {
+            document.getElementById("err_email").innerHTML =
+                "The email address contains illegal characters.";
+            return false;
+        } 
+        //password validation
+        var passwordillegalChars = /[\W_]/; // allow only letters and numbers
+        if (document.signUpForm.pwd.value === "") {
+            document.getElementById("err_password").innerHTML =
+                "You must enter a password.";
+            return false;
+        } else if ((document.signUpForm.pwd.value.length < 7) || (document.signUpForm
+            .pwd.value.length > 15)) {
+            document.getElementById("err_password").innerHTML =
+                "The password must be between 7-15 characters.";
+            return false;
+        } else if (passwordillegalChars.test(document.signUpForm.pwd.value)) {
+            document.getElementById("err_password").innerHTML =
+                "The password contains illegal characters.";
+            return false;
+        } else if (!((document.signUpForm.pwd.value.search(/(a-z)+/)) && (
+            document.signUpForm.pwd.value.search(/(0-9)+/)))) {
+            document.getElementById("err_password").innerHTML =
+                "The password must contain at least one numeral.";
+            return false;
+        } 
+        //confirm password validation
+        if (document.signUpForm.confirmpwd.value === "") {
+            document.getElementById("err_confirmPassword").innerHTML =
+                "You must confirm your password.";
+            return false;
+        } else if (document.signUpForm.confirmpwd.value != document.signUpForm.pwd
+            .value) {
+            document.getElementById("err_confirmPassword").innerHTML =
+                "The passwords should match.";
+            return false;
+        } 
+        //first name validation
+        if (document.signUpForm.firstname.value === "") {
+            document.getElementById("err_firstname").innerHTML =
+                "You must enter your first name.";
+            return false;
+        } 
+        //last name validation
+        if (document.signUpForm.lastname.value === "") {
+            document.getElementById("err_lastname").innerHTML =
+                "You must enter your last name.";
+            return false;
+        } 
+        
+        return true;
+        //document.getElementById("signUpForm").onsubmit = validateFormOnSubmit;
+    }
+    //Profile- Add Food to the Profile 
+
+function addFood() {
+        var food = document.getElementById('food').value;
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(food);
+        node.appendChild(textnode);
+        document.getElementById("foodlist").appendChild(node);
+        document.getElementById("food").value = "";
+    }
+    //Profile - Add activity to the profile
+
+function addActivity() {
+        var activity = document.getElementById('activity').value;
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(activity);
+        node.appendChild(textnode);
+        document.getElementById("activitylist").appendChild(node);
+        document.getElementById("activity").value = "";
+    }
+    //Profile - Add sleep hours to the profile
+
+function addSleep() {
+        var sleep = document.getElementById('sleep').value;
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(sleep);
+        node.appendChild(textnode);
+        document.getElementById("sleeplist").appendChild(node);
+        document.getElementById("sleep").value = "";
+    }
+    //Profile - Update Weight
+
+function addWeight() {
+    var formInput = document.getElementById('weight').value;
+    document.getElementById('newWeight').innerHTML = formInput;
+    document.getElementById("weight").value = "";
+}
+
+    //Contact us - Google Map
+    function initialize()
+{
+  var mapProp = {
+    center: new google.maps.LatLng(40.4428285,-79.9561175),
+    zoom:9,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
