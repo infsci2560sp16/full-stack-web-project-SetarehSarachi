9068b9d7167a44276e238fea4ecdebfa73332588
SetarehSarachi <setareh.sarachi@gmail.com>
2016-02-18 20:21:12 -0500

    change main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -41,12 +41,12 @@ public class Main {
     });*/
 
  
-   get("/", (request, response) -> {
+  /* get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
 
             return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
+        }, new FreeMarkerEngine());*/
 
     get("/db", (req, res) -> {
       Connection connection = null;
